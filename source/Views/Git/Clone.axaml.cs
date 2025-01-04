1d4748555dd45b5dac45b6977804c89d77a6311e
Cappecasper03 <03repsac@gmail.com>
2025-01-04 10:42:45 +0100

    A simple Clone window

---
+++ source/Views/Git/Clone.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FlameControl.Views.Git;
+
+public partial class Clone : UserControl
+{
+    public Clone( )
+    {
+        InitializeComponent();
+    }
+}
